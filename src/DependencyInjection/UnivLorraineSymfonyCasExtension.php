e9e31b16348621054547998b4f624e031d5f837f
Rafał Jelonek <rjelonek@inelo.pl>
2025-05-15 14:20:27 +0200

    Remove deprecation

--- src/DependencyInjection/UnivLorraineSymfonyCasExtension.php
+++ src/DependencyInjection/UnivLorraineSymfonyCasExtension.php
@@ -5,7 +5,7 @@ namespace UnivLorraine\Bundle\SymfonyCasBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class UnivLorraineSymfonyCasExtension extends Extension
 {
